ff4b3a883d07c5d4d608be81ed7db29c61ca12c7
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-06-10 13:45:02 -0500

    Revert "testingviewmodelforrecycler"
    This reverts commit 8447afdda51eca70b56489cad1b7022ef2e53384.

--- app/src/main/java/com/example/foodmanchu/IngredientsFragment.kt
+++ app/src/main/java/com/example/foodmanchu/IngredientsFragment.kt
@@ -5,11 +5,8 @@ import android.view.View
 import android.widget.Adapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodmanchu.databinding.IngredientsFragmentBinding
-import java.math.RoundingMode
 
 class IngredientsFragment:Fragment(R.layout.ingredients_fragment) {
 
@@ -21,23 +18,11 @@ class IngredientsFragment:Fragment(R.layout.ingredients_fragment) {
 
         ingredientsAdapter = IngredientsAdapter()
 
-        val ingredientsViewModel = ViewModelProvider(
-                this).get(IngredientsViewModel::class.java)
-
-        val liveIngredients = ingredientsViewModel.liveIngredientsList
-
-
-
         binding.apply {
             ingredientsRecyclerview.apply{
-                liveIngredients.observe(viewLifecycleOwner, Observer {
-                    adapter = ingredientsAdapter
-                    layoutManager = LinearLayoutManager(requireContext())
-                    ingredientsAdapter.submitList(it)
-                    ingredientsAdapter.notifyDataSetChanged()
-
-                })
-
+                adapter = ingredientsAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                ingredientsAdapter.submitList(Repository.IngredientsList)
             }
 
             addIngredientFab.setOnClickListener{
@@ -73,7 +58,7 @@ class IngredientsFragment:Fragment(R.layout.ingredients_fragment) {
 
     }
 
- 
+
 
     fun updateAdapter(){
         ingredientsAdapter.notifyDataSetChanged()
