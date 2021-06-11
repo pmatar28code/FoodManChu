921f31fc4319dba38b8919649b81aaa6808e7b6f
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-06-10 20:51:34 -0500

    cleaning up a bit , ingredient selection for recipe still work in progress
    cleaning up a bit , ingredient selection for recipe still work in progress

--- app/src/main/java/com/example/foodmanchu/AddRecipeDialog.kt
+++ app/src/main/java/com/example/foodmanchu/AddRecipeDialog.kt
@@ -48,15 +48,13 @@ class AddRecipeDialog:DialogFragment() {
         lateinit var dialog:AlertDialog
 
         // Initialize an array of colors
-        var index = 0
-        var arrayColors:Array<String> = Repository.IngredientsList.map { it.ingredientName }.toTypedArray()
+        var arrayIngredients:Array<String> = Repository.IngredientsList.map { it.ingredientName }.toTypedArray()
 
         var mutableListFalse = mutableListOf<Boolean>()
         for(ingredient in Repository.IngredientsList){
             mutableListFalse.add(false)
         }
 
-        index = 0
         // Initialize a boolean array of checked items
         var arrayChecked:BooleanArray = mutableListFalse.map { false }.toBooleanArray()
 
@@ -67,57 +65,29 @@ class AddRecipeDialog:DialogFragment() {
         // Set a title for alert dialog
         builder.setTitle("Choose favorite colors.")
 
-        /*
-            **** reference source developer.android.com ***
 
-            AlertDialog.Builder setMultiChoiceItems (CharSequence[] items,
-                            boolean[] checkedItems,
-                            DialogInterface.OnMultiChoiceClickListener listener)
-
-            Set a list of items to be displayed in the dialog as the content, you will be notified
-            of the selected item via the supplied listener. The list will have a check mark
-            displayed to the right of the text for each checked item. Clicking on an item
-            in the list will not dismiss the dialog. Clicking on a button will dismiss the dialog.
-
-            Parameters
-                items CharSequence : the text of the items to be displayed in the list.
-
-                checkedItems boolean : specifies which items are checked. It should be null in which
-                    case no items are checked. If non null it must be exactly the same length
-                    as the array of items.
-
-                listener DialogInterface.OnMultiChoiceClickListener : notified when an item on the
-                    list is clicked. The dialog will not be dismissed when an item is clicked. It
-                    will only be dismissed if clicked on a button, if no buttons are supplied
-                    it's up to the user to dismiss the dialog.
-
-            Returns
-                AlertDialog.Builder This Builder object to allow for chaining of calls to set methods
-        */
 
         // Define multiple choice items for alert dialog
-        builder.setMultiChoiceItems(arrayColors, arrayChecked, {dialog,which,isChecked->
+        builder.setMultiChoiceItems(arrayIngredients, arrayChecked) { dialog, which, isChecked->
             // Update the clicked item checked status
-            arrayChecked?.set(which, isChecked)
+            arrayChecked[which] = isChecked
 
             // Get the clicked item
-            val color = arrayColors?.get(which)
+            val color = arrayIngredients[which]
 
             // Display the clicked item text
             //toast("$color clicked.")
-        })
+        }
 
 
         // Set the positive/yes button click listener
         builder.setPositiveButton("OK") { _, _ ->
             // Do something when click positive button
             binding.textView.text = "Your preferred colors..... \n"
-            if (arrayColors != null) {
-                for (i in 0 until arrayColors.size) {
-                    val checked = arrayChecked?.get(i)
-                    if (checked!!) {
-                        binding.textView.text = "${binding.textView.text}  ${arrayColors[i]} \n"
-                    }
+            for (i in arrayIngredients.indices) {
+                val checked = arrayChecked?.get(i)
+                if (checked!!) {
+                    binding.textView.text = "${binding.textView.text}  ${arrayIngredients[i]} \n"
                 }
             }
         }
