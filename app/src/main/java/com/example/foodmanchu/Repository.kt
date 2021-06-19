71575babfd32a9bd4feea16a5187e48d673342e4
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-06-18 21:08:41 -0500

    changed default recipe to include larger instructions, added instructions as string

--- app/src/main/java/com/example/foodmanchu/Repository.kt
+++ app/src/main/java/com/example/foodmanchu/Repository.kt
@@ -169,12 +169,12 @@ object Repository {
 
     var defaultRecipesList = mutableListOf<Recipes>(
             Recipes(
-                    recipeName = "scrambled Eggs",
-                    ingredientsToUse = "egg,salt",
-                    description = "delicious scrambled eggs",
-                    cookingInstructions = "mix to eggs,add salt,cook for 3.5 minutes,enjoy",
-                    prepTime = "4 minutes",
-                    recipeCategory = "Keto",
+                    recipeName = "Lasagna",
+                    ingredientsToUse = "garlic,salt,meat,tomato,white_cheese",
+                    description = "delicious lasagna",
+                    cookingInstructions = "To assemble, spread 1 1/2 cups of meat sauce in the bottom of a 9x13-inch baking dish. Arrange 6 noodles lengthwise over meat sauce. Spread with one half of the ricotta cheese mixture. Top with a third of mozzarella cheese slices. Spoon 1 1/2 cups meat sauce over mozzarella, and sprinkle with 1/4 cup Parmesan cheese. Repeat layers, and top with remaining mozzarella and Parmesan cheese. Cover with foil: to prevent sticking, either spray foil with cooking spray, or make sure the foil does not touch the cheese Bake in preheated oven for 25 minutes. Remove foil, and bake an additional 25 minutes. Cool for 15 minutes before serving.",
+                    prepTime = "45 minutes",
+                    recipeCategory = "Meat",
                     recipeImage = ""
             )
     )
